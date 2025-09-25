2dee9c74e2439c22df4091691dc834509a99cf56
Andrewc0d3 <davidsanchez102502@gmail.com>
2025-09-25 21:21:39 +0000

    sfgdsfgd

--- js/Pokemon.js
+++ js/Pokemon.js
@@ -1,10 +1,18 @@
 async function Pokemon (){
 
-    var root = document.getElementById("root");
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/"+id);
     const data= await res.json();
     
     console.log(data)
+    var root =  document.getElementById("root");
+
+    let tipoPoke = "";
+   for(let i = 0; i <data.types.length; i++) {
+        tipoPoke += '<span>${data.types[i].type.name}</span>';
+    }
+
+    root.innerHTML= ^div class^
 }
 
-Pokemon ()
+Pokemon(5)
+Pokemon(254)
