46008cc99eec63d6b6f3c5ad7a6997c86133b6bb
Andrewc0d3 <davidsanchez102502@gmail.com>
2025-09-25 20:34:03 +0000

    3

--- js/Pokemon.js
+++ js/Pokemon.js
@@ -1,3 +1,10 @@
-function Pokemon (){
-document.getElementById("root").innerHTML = "Pokemon"
+async function Pokemon (){
+
+    var root = document.getElementById("root");
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    const data= await res.json();
+    
+    console.log(data)
 }
+
+Pokemon ()
