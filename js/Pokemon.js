b2880ed5cb38053afed180abc096cd59f79ddbff
Andrewc0d3 <davidsanchez102502@gmail.com>
2025-10-02 18:30:26 +0000

    wa

--- js/Pokemon.js
+++ js/Pokemon.js
@@ -10,9 +10,21 @@ async function Pokemon (){
    for(let i = 0; i <data.types.length; i++) {
         tipoPoke += '<span>${data.types[i].type.name}</span>';
     }
+    const detalle = `
+    <section class="c-detalle">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="${data.name}" height="120" width="auto">
+        <p>${data.name}</p>
+        <p>${data.id}</p>
+        <p>Altura: ${data.height / 10} m / Peso: ${data.weight / 10} kg</p>
+        <p>hp: ${data.stats[0].base_stat}</p>
+        <p>Velocidad: ${data.stats[5].base_stat}</p>
+        <p>Ataque: ${data.stats[1].base_stat} Defensa: ${data.stats[2].base_stat}</p>
+        <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.stats[4].base_stat}</p>
 
-    root.innerHTML= ^div class^
+    </section>
+    `;
+
+    root.innerHTML= detalle
 }
 
 Pokemon(5)
-Pokemon(254)
